2471455e324ac155b1ecec69dc06aca5734d1056
Chee Ng <cwng@ncsu.edu>
2021-02-04 16:01:28 -0500

    fetching initial post

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log('First middleware')
-    next();
-});
+app.use('/api/posts', (req, res, next) =>{
+    const posts = [
+        {id: '1001', title: 'Dummy #1', content: 'Data from server'},
+        {id: '1002', title: 'Dummy #2', content: 'Data from server'}
+    ];
+    res.json({
+        message: 'Posts fetched',
+        posts: posts
+    });
 
-
-app.use((req, res, next) =>{
-    res.send('Hello')
 });
 
 module.exports = app;
