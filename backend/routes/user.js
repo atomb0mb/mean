8e4e192b26fe5743ee3d4a5d15622cc22f9d31c6
Chee Ng <cwng@ncsu.edu>
2021-02-25 14:04:01 -0500

    Added New user upon request

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 const router = express.Router();
 
-router.post('/signup')
+router.post('/signup', (res, req, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash,
+
+        })
+        user.save() // save to the database
+        .then(result => {
+            res.status(201).json({
+                message: 'User created!',
+                result: result
+            });
+        }) 
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+    })
+})
 
 module.exports = router;
