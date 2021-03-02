4a441efc5e164250ecff03e5cfe9b6aee803e1f5
Chee Ng <cwng@ncsu.edu>
2021-03-02 14:13:19 -0500

    Add custom dialog error display from Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -19,12 +20,12 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostService } from './posts/post.service';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './auth/error/error.component';
 
 
 
@@ -36,6 +37,7 @@ import { ErrorInterceptor } from './error-interceptor';
     PostListComponent,
     LoginComponent,
     SignupComponent,
+    ErrorComponent
   ],
   imports: [
     FormsModule,
@@ -48,6 +50,7 @@ import { ErrorInterceptor } from './error-interceptor';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     MatToolbarModule,
     MatExpansionModule,
     MatPaginatorModule,
@@ -59,6 +62,7 @@ import { ErrorInterceptor } from './error-interceptor';
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}, 
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}],
   
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent] // Tell angular uses it even though it can't see it
 })
 export class AppModule { }
--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -1,17 +1,24 @@
 import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { ErrorComponent } from "./auth/error/error.component";
 
-
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    constructor(private dialog: MatDialog){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
     
         return next.handle(req).pipe(
             catchError((error: HttpErrorResponse) => {
-                console.log(error);
+                
                 // to display the default error message = alert(error.error.error.message)
-                alert(error.error.message);
+                
+                this.dialog.open(ErrorComponent);
+
                 return throwError(error); 
             })
         );
