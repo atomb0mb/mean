12890b52845bc628a4683d1647d16aa68a4eebc8
Chee Ng <cwng@ncsu.edu>
2021-03-03 18:09:00 -0500

    Adding lazy loading for login and signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,17 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './auth/error/error.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { PostModule } from './posts/post.module';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -22,18 +20,16 @@ import { PostModule } from './posts/post.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
-    PostModule,
-    FormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    AuthModule,
+    PostModule,
     
   ],
   providers: [
