1debed5ff8be95f5793cb46bcd7ef4f8369c42c4
Chee Ng <cwng@ncsu.edu>
2021-03-03 16:28:41 -0500

    Re-organize module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
-
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,6 +15,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './auth/error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 
 
@@ -46,16 +36,7 @@ import { ErrorComponent } from './auth/error/error.component';
     HttpClientModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatPaginatorModule,
-    MatProgressSpinnerModule,
-
+    AngularMaterialModule,
     
   ],
   providers: [
